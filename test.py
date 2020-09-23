7f072f1da2f0eaebcbbb6750ac96d3e1eeccaa99
Thorasine <zajakrisz@gmail.com>
2020-09-23 13:38:50 +0200

    Reverts deletions

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import time
 
 f = open("resources/00inactives.txt", "w+")
 lol_watcher = LolWatcher('RGAPI-8e55f644-1da7-4a40-be9c-83beaea42b3d')
+print("Hey")
 
 for i in range(2):
     summoner = lol_watcher.summoner.by_name('euw1', 'Thorasine')
